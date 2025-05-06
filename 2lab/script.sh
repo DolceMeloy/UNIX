778dfe6628f0be1a808f8aec109815096ff78a80
DolceMeloy <159237077+DolceMeloy@users.noreply.github.com>
2025-05-06 18:37:19 +0700

    add 2 lab

--- 2lab/script.sh
+++ 2lab/script.sh
@@ -4,15 +4,15 @@ ID=$(uuidgen)
 seq=1
 while true; do
 	filename=$(
-	flock -x 200
+	flock -x 199
 	N=1
-while [ -e "/shared/$(printf "%03d" $N)" ]; do
+while ( -e "/shared/$(printf "%03d" $N)" ); do
 	N=$(expr $N + 1)
 done
 filename="/shared/$(printf "%03d" $N)"
 touch "$filename"
 echo "$filename"
-) 200>/shared/lockfile
+) 199>/shared/lockfile
 echo "identifier: $ID" > "$filename"
 echo "sequence: $seq" >> "$filename"
 echo "$(date '+%Y-%m-%d %H:%M:%S'): Created file $filename with ID $ID and sequence $seq"
